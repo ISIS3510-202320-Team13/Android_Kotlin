62767aff3a47f50c890b35c29bed43e37cc0a3c3
sebcaceres <64490370+sebcaceres@users.noreply.github.com>
2023-09-19 02:33:45 -0500

    Version of sdk solved sdk 34 recommended

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.parkezkotlin"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.parkezkotlin"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
