385783f934089e0b2ff3ea44a2df632ab68258ad
sebcaceres <64490370+sebcaceres@users.noreply.github.com>
2023-10-03 05:10:35 -0500

    fixing login form

--- app/src/main/java/com/example/parkezkotlin/ui/login/signUpFragment.kt
+++ app/src/main/java/com/example/parkezkotlin/ui/login/signUpFragment.kt
@@ -19,6 +19,7 @@ import androidx.navigation.Navigation
 import com.example.parkezkotlin.databinding.FragmentSignUpBinding
 
 import com.example.parkezkotlin.R
+import com.google.firebase.auth.FirebaseAuth
 
 class signUpFragment : Fragment() {
 
@@ -27,6 +28,7 @@ class signUpFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,7 +46,43 @@ class signUpFragment : Fragment() {
         val backButton = binding.imageButton
         backButton.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_signUpFragment2_to_main)
-        }}
+        }
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val emailEditText = binding.username
+        val emailConfirmEditText = binding.username2
+        val passwordEditText = binding.password
+        val confirmPasswordEditText = binding.password2
+        val signUpButton = binding.signup
+        if (emailEditText.text.toString().isEmpty() || emailConfirmEditText.text.toString().isEmpty()) {
+            emailEditText.error = "Please enter email"
+            emailEditText.requestFocus()
+        }
+        if (passwordEditText.text.toString().isEmpty() || confirmPasswordEditText.text.toString().isEmpty()) {
+            passwordEditText.error = "Please enter password"
+            passwordEditText.requestFocus()
+        }
+        if (passwordEditText.text.toString() != confirmPasswordEditText.text.toString()) {
+            confirmPasswordEditText.error = "Passwords do not match"
+            confirmPasswordEditText.requestFocus()
+        }
+        if (emailEditText.text.toString() != emailConfirmEditText.text.toString()) {
+            emailConfirmEditText.error = "Emails do not match"
+            emailConfirmEditText.requestFocus()
+        }
+        signUpButton.setOnClickListener {
+            firebaseAuth.createUserWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString()).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(context, "User Created", Toast.LENGTH_SHORT).show()
+                    Navigation.findNavController(view).navigate(R.id.action_signUpFragment2_to_main)
+                } else {
+                    Toast.makeText(context, "Error: ${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
