afb9befa623f0ea68617b96d6439e5150a83e6aa
Sebastian Caceres <89409633+jcacerescc@users.noreply.github.com>
2023-10-11 18:25:46 -0500

    marker

--- app/src/main/java/com/example/parkezkotlin/MapsFragment.kt
+++ app/src/main/java/com/example/parkezkotlin/MapsFragment.kt
@@ -1,50 +1,108 @@
 package com.example.parkezkotlin
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
+import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.parkezkotlin.databinding.FragmentMapsBinding
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
-
 
 class MapsFragment : Fragment() {
     private var _binding: FragmentMapsBinding? = null
     private val binding get() = _binding!!
+    private var googleMap: GoogleMap? = null
+    private var marker: Marker? = null
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        // access to the user location
+    @SuppressLint("MissingPermission")
+    private val callback = OnMapReadyCallback { map ->
+        googleMap = map
 
+        // Check for location permissions
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permissions are granted; enable location features
+            map.isMyLocationEnabled = true
 
-        val bogota = LatLng(4.60971, -74.08175)
-        googleMap.addMarker(MarkerOptions().position(bogota).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(bogota))
-        //zoom ti the marker position
+            val bogota = LatLng(4.60971, -74.08175)
+            map.addMarker(MarkerOptions().position(bogota).title("Marker in Bogota"))
 
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(bogota, 12f))
+            // Get the user's current location and add a marker there
+            getCurrentLocation { location ->
+                location?.let {
+                    val userLocation = LatLng(location.latitude, location.longitude)
+                    map.addMarker(MarkerOptions().position(userLocation).title("Your Location"))
+                    map.moveCamera(CameraUpdateFactory.newLatLng(userLocation))
+                }
+            }
+        } else {
+            // Permissions are not granted; request location permissions
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                PERMISSION_REQUEST_CODE
+            )
+        }
+
+        // Set a click listener to add a marker where the user taps
+        map.setOnMapClickListener { latLng ->
+            addMarker(latLng)
+        }
     }
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    private fun getCurrentLocation(callback: (LatLng?) -> Unit) {
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    callback(LatLng(location.latitude, location.longitude))
+                } else {
+                    Log.e(TAG, "Location not found")
+                    callback(null)
+                }
+            }
+        }
+    }
+
+    private fun addMarker(latLng: LatLng) {
+        // Remove the previous marker, if it exists
+        marker?.remove()
+
+        // Add a new marker at the specified LatLng position
+        marker = googleMap?.addMarker(MarkerOptions().position(latLng).title("User Added Marker"))
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
@@ -52,30 +110,15 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
-        /*val button = binding.imageButton5
-        button.setOnClickListener(){
-            Navigation.findNavController(view).navigate(R.id.action_mapsFragment_to_booking_info)
-        }
-        val button2 = binding.imageButton4
-        button2.setOnClickListener(){
-            Navigation.findNavController(view).navigate(R.id.action_mapsFragment_to_parkingDetail)
-        }
 
-
-        button2.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_main_to_signUpFragment2)
-        }
-        */
-
-
-        //load and apply custom style
+        // Load and apply custom map style
         try {
-            val success = mapFragment?.getMapAsync { googleMap ->
+            val success = mapFragment?.getMapAsync { map ->
                 val mapStyleOptions = MapStyleOptions.loadRawResourceStyle(
                     requireContext(),
                     R.raw.map_style
                 )
-                googleMap.setMapStyle(mapStyleOptions)
+                map.setMapStyle(mapStyleOptions)
             }
             if (success == null) {
                 Log.e(TAG, "Style parsing failed.")
@@ -83,12 +126,9 @@ class MapsFragment : Fragment() {
         } catch (e: Resources.NotFoundException) {
             Log.e("MapsFragment", "Raw resource for map style not found: $e")
         }
+    }
 
-
+    companion object {
+        private const val PERMISSION_REQUEST_CODE = 123 // You can use any request code
     }
 }
-
-
-
-
-
