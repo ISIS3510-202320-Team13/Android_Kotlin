2fe0bccc4d2ffb381e3c5ec3ba44d41ca18520d4
nFei <57652524+nFei@users.noreply.github.com>
2023-12-04 17:57:48 -0500

    Added parking lot names and cache for reservations

--- app/src/main/java/com/example/parkezkotlin/ui/viewModel/ReservationViewModel.kt
+++ app/src/main/java/com/example/parkezkotlin/ui/viewModel/ReservationViewModel.kt
@@ -4,6 +4,7 @@ package com.example.parkezkotlin.ui.viewModel
 
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -15,10 +16,14 @@ import kotlinx.coroutines.tasks.await
 
 class ReservationViewModel : ViewModel() {
     val reservationsLiveData = MutableLiveData<List<Reservation>?>()
-    val reservationDetailLiveData = MutableLiveData<Reservation?>()
-    val parkingDetailLiveData = MutableLiveData<parkingModel?>()
+    val reservationDetailLiveData = MutableLiveData<Pair<String, Reservation?>?>()
     private val db = FirebaseFirestore.getInstance()
-    private val TAG = "ReservationViewModel"  // Ensure TAG is initialized
+    private val TAG = "ReservationViewModel"
+
+    // Cache that stores reservation details using reservation ID
+    val reservationDetailsCache = mutableMapOf<String, Reservation>()
+
+    private val parkingLotNamesMap = MutableLiveData<Map<String, String>>()
 
     fun fetchReservationsFromUser(userId: String) {
         viewModelScope.launch {
@@ -36,6 +41,10 @@ class ReservationViewModel : ViewModel() {
                     Log.d(TAG, "Fetched reservations: $reservations") // Log the fetched data
 
                     reservationsLiveData.postValue(reservations)
+                    // Cache reservation details
+                    reservations.forEach { reservation ->
+                        reservationDetailsCache[reservation.uid ?: ""] = reservation
+                    }
                 }
             } catch (exception: Exception) {
                 Log.w(TAG, "Error getting reservations from network: ", exception)
@@ -44,23 +53,55 @@ class ReservationViewModel : ViewModel() {
         }
     }
 
-    fun fetchReservationDetails(reservationId: String) {
-        val docRef = db.collection("reservations").document(reservationId)
-
-        docRef.get()  // This will fetch from the network by default
-            .addOnSuccessListener { snapshot ->
-                if (snapshot.exists()) {
-                    val reservationDetail = snapshot.toObject(Reservation::class.java)
-                    reservationDetailLiveData.postValue(reservationDetail)
-                } else {
-                    Log.d(TAG, "No such document in the network")
-                    reservationDetailLiveData.postValue(null)
+    fun fetchParkingLotNames() {
+        db.collection("parkings")
+            .get()
+            .addOnSuccessListener { documents ->
+                val map = mutableMapOf<String, String>()
+                for (document in documents) {
+                    val parkingLotId = document.id
+                    val parkingLotName = document.getString("name") ?: ""
+                    map[parkingLotId] = parkingLotName
                 }
+                parkingLotNamesMap.postValue(map)
             }
             .addOnFailureListener { exception ->
-                Log.w(TAG, "Error getting parking details from network: ", exception)
-                reservationDetailLiveData.postValue(null)
+                Log.w(TAG, "Error getting parking lot names: ", exception)
             }
     }
 
+    fun getParkingLotNamesMap(): LiveData<Map<String, String>> {
+        return parkingLotNamesMap
+    }
+
+
+
+    fun fetchReservationDetails(reservationId: String) {
+        if (reservationDetailsCache.containsKey(reservationId)) {
+            // If the details are cached, retrieve from the cache
+            val cachedReservation = reservationDetailsCache[reservationId]
+            reservationDetailLiveData.postValue(Pair(reservationId, cachedReservation))
+        } else {
+            // Fetch details from Firestore if not cached
+            val docRef = db.collection("reservations").document(reservationId)
+            docRef.get()
+                .addOnSuccessListener { snapshot ->
+                    if (snapshot.exists()) {
+                        val reservationDetail = snapshot.toObject(Reservation::class.java)
+                        reservationDetailLiveData.postValue(Pair(reservationId, reservationDetail))
+                        reservationDetail?.let {
+                            reservationDetailsCache[reservationId] = it // Cache the fetched details
+                        }
+                    } else {
+                        Log.d(TAG, "No such document in the network")
+                        reservationDetailLiveData.postValue(Pair(reservationId, null))
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting reservation details from network: ", exception)
+                    reservationDetailLiveData.postValue(Pair(reservationId, null))
+                }
+        }
+    }
 }
+
