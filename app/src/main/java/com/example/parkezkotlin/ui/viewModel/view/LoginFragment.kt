45c8c3e8ea904bb759497242883f9cd9dd809800
sebcaceres <64490370+sebcaceres@users.noreply.github.com>
2023-12-03 22:46:05 -0500

    Sending date to the back when a reservation is created,

--- app/src/main/java/com/example/parkezkotlin/ui/viewModel/view/LoginFragment.kt
+++ app/src/main/java/com/example/parkezkotlin/ui/viewModel/view/LoginFragment.kt
@@ -43,6 +43,7 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
